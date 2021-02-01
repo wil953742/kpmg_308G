316af540f9b78ecc90b61b044a427a4156977750
KORguy <hyun.kang@hotmail.com>
2021-02-01 14:20:13 +0900

    Feb 1.
    TypeScript Set Up

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function home() {
+  return (
+    <>
+      <Head>
+        <title>temporary title</title>
+      </Head>
+      <body>
+        <h1>Hello</h1>
+      </body>
+    </>
+  );
+}
