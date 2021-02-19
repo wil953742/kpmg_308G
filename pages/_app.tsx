65d57a81632714e45ec019aee675170a3a1f46ed
KORguy <hyun.kang@hotmail.com>
2021-02-20 00:23:29 +0900

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,11 @@
 import GlobalStyle from "../GlobalStyle";
-import { Provider } from "next-auth/client";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Provider session={pageProps.session}>
+    <>
       <GlobalStyle />
       <Component {...pageProps} />
-    </Provider>
+    </>
   );
 }
 
